8f8627ddce69557d8477f440a162198bc4ec9ca6
Leon Arndt <leon.arndt@innogames.com>
2024-04-05 21:12:11 +0200

    add pedestrians

--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -5,14 +5,22 @@ public class Vehicle : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent agent;
     [SerializeField] private Vector3 direction = Vector3.forward;
-    
-    [Range(1f, 5f)]
-    [SerializeField]
-    private float maxRandomSpeedMultiplier = 1.5f;
+
+    [Range(1f, 5f)] [SerializeField] private float maxRandomSpeedMultiplier = 1.5f;
+
+    private static readonly int Speed = Animator.StringToHash("Speed");
+    private static readonly int MotionSpeed = Animator.StringToHash("MotionSpeed");
 
     private void Start()
     {
-        agent.speed *= UnityEngine.Random.Range(1f, maxRandomSpeedMultiplier);
+        var speed = UnityEngine.Random.Range(1f, maxRandomSpeedMultiplier);
+        agent.speed *= speed;
+        if (TryGetComponent<Animator>(out var anim))
+        {
+            anim.SetFloat(MotionSpeed, 1.3f);
+            anim.SetFloat(Speed, speed * 1.3f);
+        }
+
         Destroy(gameObject, 50f);
 
         if (Random.value > 0.5f)
