b61cc612dd114fecaf07b10ff1dcf068cf66811e
Leon Arndt <leon.arndt@innogames.com>
2024-04-19 17:47:03 +0200

    add pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private Button continueButton;
+    private void Awake()
+    {
+        pausePanel.SetActive(false);
+        continueButton.onClick.AddListener(() => SetPausedStatus(false));
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            var wasPreviouslyPaused = pausePanel.activeSelf;
+            SetPausedStatus(!wasPreviouslyPaused);
+        }
+    }
+
+    private void SetPausedStatus(bool isPaused)
+    {
+        pausePanel.SetActive(isPaused);
+        Time.timeScale = isPaused ? 0 : 1;
+        Cursor.visible = isPaused;
+        Cursor.lockState = isPaused ? CursorLockMode.None : CursorLockMode.Locked;
+    }
+}
