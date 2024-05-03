8200202cc141dbe2e0a192eec23e9183c100486e
Leon Arndt <leon.arndt@innogames.com>
2024-05-03 13:43:18 +0200

    animate nav mesh followers depending on their agent speeds

--- Assets/Scripts/NavMeshFollower.cs
+++ Assets/Scripts/NavMeshFollower.cs
@@ -8,15 +8,24 @@ public class NavMeshFollower : MonoBehaviour
     [SerializeField] private float minDistance = 5f;
     [SerializeField] private float maxDistance = 50f;
 
+    private static readonly int Speed = Animator.StringToHash("Speed");
+    private static readonly int MotionSpeed = Animator.StringToHash("MotionSpeed");
+
     private void Update()
     {
+        if (TryGetComponent<Animator>(out var anim))
+        {
+            anim.SetFloat(MotionSpeed, 1.3f);
+            anim.SetFloat(Speed, agent.velocity.magnitude);
+        }
+
         var distance = Vector3.Distance(transform.position, target.position);
         if (distance < minDistance)
         {
             agent.isStopped = true;
             return;
         }
-        
+
         if (distance > maxDistance)
         {
             agent.isStopped = true;
