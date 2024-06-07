84789fec3122c82b29414fcb2b8724cc68705640
Leon Arndt <leon.arndt@innogames.com>
2024-06-07 14:15:16 +0200

    fix quest system requirements

--- Assets/Scripts/QuestSystem.cs
+++ Assets/Scripts/QuestSystem.cs
@@ -10,25 +10,32 @@ public static class QuestSystem
         {
             if (quest.Quest is CollectionQuest collectionQuest)
             {
-                foreach (var requirement in collectionQuest.requirements)
-                {
-                    if (GameState.GetAllItems().TryGetValue(requirement.type, out var itemAmount))
-                    {
-                        if (itemAmount < requirement.amount)
-                        {
-                            return;
-                        }
-                    }
-                    else
-                    {
-                        return;
-                    }
-                }
-                
-                GameState.MarkQuestCompletable(collectionQuest);
+                UpdateQuest(collectionQuest);
             }
         }
 
         Object.FindObjectOfType<QuestLogView>(true).ShowActiveQuests();
+    }
+
+    private static void UpdateQuest(CollectionQuest collectionQuest)
+    {
+        foreach (var requirement in collectionQuest.requirements)
+        {
+            if (GameState.GetAllItems().TryGetValue(requirement.type, out var itemAmount))
+            {
+                if (itemAmount < requirement.amount)
+                {
+                    // not enough itemAmount
+                    return;
+                }
+            }
+            else
+            {
+                // don't have item at all
+                return;
+            }
+        }
+
+        GameState.MarkQuestCompletable(collectionQuest);
     }
 }
